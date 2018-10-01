cff28a9ed7dcebb220b76ced9730a706d51ac141
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-01 08:52:38 -0400

    Update message.py

--- message.py
+++ message.py
@@ -4,9 +4,9 @@ class message():
     def __init__(self, bot):
         self.bot = bot
         
-async def on_message(self, message):
-    if 'hi chara' or 'Hi chara' or 'hi Chara' or 'Hi Chara' or 'hello chara' or 'Hello chara' or 'Hello Chara' or 'hello Chara'
-    await self.bot.send_message(message.channel, "Hello")
+    async def on_message(self, message):
+        if 'hi chara' or 'Hi chara' or 'hi Chara' or 'Hi Chara' or 'hello chara' or 'Hello chara' or 'Hello Chara' or 'hello Chara' in message.content:
+           await self.bot.send_message(message.channel, "Hello")
 
 
 
