cba6c1c990a6b69057a345f8429e6031cc839f1c
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-01 08:50:01 -0400

    Update message.py

--- message.py
+++ message.py
@@ -3,6 +3,10 @@ import discord
 class message():
     def __init__(self, bot):
         self.bot = bot
+        
+async def on_message(self, message):
+    if 'hi chara' or 'Hi chara' or 'hi Chara' or 'Hi Chara' or 'hello chara' or 'Hello chara' or 'Hello Chara' or 'hello Chara'
+    await self.bot.send_message(message.channel, "Hello")
 
 
 
