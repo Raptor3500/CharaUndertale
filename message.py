093d014b6d564ff0ddf8fe089ee7aeb9c3f9b33e
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-09-30 20:33:03 -0400

    Update message.py

--- message.py
+++ message.py
@@ -5,8 +5,8 @@ class message():
         self.bot = bot
 
     async def on_message(self, message):
-        if 'pervert' in message.content:
-            await self.bot.send_message(message.channel, "I'll burn him to cinders")
+        if 'Hi' or 'hi' in message.content:
+            await self.bot.send_message(message.channel, "Hi")
 
 
 
