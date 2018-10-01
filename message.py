1b4645b702739a1cc270f9a7dda48e2c4cab40f5
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-01 08:56:54 -0400

    Update message.py

--- message.py
+++ message.py
@@ -3,10 +3,10 @@ import discord
 class message():
     def __init__(self, bot):
         self.bot = bot
-        
+
     async def on_message(self, message):
-        if 'hi chara' or 'Hi chara' or 'hi Chara' or 'Hi Chara' or 'hello chara' or 'Hello chara' or 'Hello Chara' or 'hello Chara' in message.content:
-           await self.bot.send_message(message.channel, "Hey")
+        if 'pervert' in message.content:
+            await self.bot.send_message(message.channel, "I'll burn him to cinders")
 
 
 
